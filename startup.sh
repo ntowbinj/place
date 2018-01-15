6fcce673e36c6d0faf5914f9ce210902d0f964b8
n <n>
2018-01-15 07:23:52 -0500

    add -D flag

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/gunicorn --worker-connections 1000 -b 127.0.0.1:8090 wsgi --access-logfile /var/log/hearit-accesslog.log
+/usr/local/bin/gunicorn -D --worker-connections 1000 -b 127.0.0.1:8090 wsgi --access-logfile /var/log/hearit-accesslog.log
 
