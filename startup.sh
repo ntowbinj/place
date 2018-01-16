50950e4a6aae204b6105685eba6719a8bb61dc47
n <n>
2018-01-15 22:13:29 -0500

    hyphen

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/gunicorn -D --worker-connections 1000 -b 127.0.0.1:8090 wsgi --access-logfile /var/log/hearit-accesslog.log --log-file /var/log/hearit-app.log -error-logfile /var/log/hearit-error.log
+/usr/local/bin/gunicorn -D --worker-connections 1000 -b 127.0.0.1:8090 wsgi --access-logfile /var/log/hearit-accesslog.log --log-file /var/log/hearit-app.log --error-logfile /var/log/hearit-error.log
 
