ca823a7a3916618195d0295cd29f223cb7eed3d7
n <n>
2018-02-04 20:39:51 -0500

    fix startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
-/usr/local/bin/gunicorn -D --worker-connections 1000 -b 127.0.0.1:8090 wsgi --access-logfile /var/log/hearit-accesslog.log --log-file /var/log/hearit-app.log --error-logfile /var/log/hearit-error.log
+MY_PATH="`dirname \"$0\"`"
+cd $MY_PATH
+/usr/local/bin/gunicorn --reload -D --worker-connections 1000 -b 127.0.0.1:8090 wsgi --access-logfile /var/log/hearit-accesslog.log --log-file /var/log/hearit-app.log --error-logfile /var/log/hearit-error.log
 
